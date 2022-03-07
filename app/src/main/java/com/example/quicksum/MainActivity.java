9dedab19b7818e3e9761c64756aa4d750afa18c8
DaRealTaylor <taylor.fontes@my.jcu.edu.au>
2022-03-07 10:45:21 +1000

    Add code to MainActivity.java

--- app/src/main/java/com/example/quicksum/MainActivity.java
+++ app/src/main/java/com/example/quicksum/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.quicksum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int sum;
+    private int result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void buttonClicked(View view){
+        Button button = (Button) view;
+        
+        int number = Integer.parseInt(button.getText().toString());
+        sum += number;
+
+        TextView textView = findViewById(R.id.sum);
+        textView.setText(result);
     }
 }
